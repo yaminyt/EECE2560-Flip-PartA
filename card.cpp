9955811f4d27a2410231d5153dc66853f7b0a367
Yamin  Mahmood <128117506+yaminyt@users.noreply.github.com>
2026-02-17 13:44:08 -0500

    Add card.cpp

---
+++ card.cpp
@@ -0,0 +1,45 @@
+// ===== src/card.cpp =====
+#include "card.h"
+
+card::card(int v, int s)
+{
+   value = v;
+   suit = s;
+}
+
+void card::setValue(int v)
+{
+   value = v;
+}
+
+void card::setSuit(int s)
+{
+   suit = s;
+}
+
+int card::getValue() const
+{
+   return value;
+}
+
+int card::getSuit() const
+{
+   return suit;
+}
+
+std::ostream &operator<<(std::ostream &out, const card &c)
+{
+   const char *vals[] =
+   {
+      "A","2","3","4","5","6","7","8","9","10","J","Q","K"
+   };
+
+   const char *suits[] =
+   {
+      "C","D","H","S"
+   };
+
+   out << vals[c.getValue() - 1] << suits[c.getSuit()];
+   return out;
+}
+
